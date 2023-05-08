110293c47cb39ae601b4cf0e7cffb0678929c6a4
Qian <wood123487@gmail.com>
2023-05-08 22:11:00 +0800

    flask上傳檔案給伺服器測試

---
+++ flask_upfile/test.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app=Flask(__name__)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        data_xls = pd.read_excel(f)
+        return data_xls.to_html()
+    return '''
+    <!doctype html>
+    <title>Upload an excel file</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+    </form>
+    '''
+
+if __name__ == "__main__":
+    app.run()
